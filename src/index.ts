12904c13ffcbb622d213599c25d315f953728e42
David Mesquita-Morris <mesmo@steelbreeze.net>
2022-02-08 07:31:19 +0000

    Replace null for uTidyndefine

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ export const cube = <TRow>(table: Array<TRow>, axes: Axes<TRow>): Cube<TRow> =>
  * Generates a function to slice data by the criteria specified in a dimension.
  * @param dimension The dimension to generate the slicer for.
  * @returns Returns a function that will take a table and slice it into an array of tables each conforming to the criteria of a point on a dimension.
-*/
+ */
 export const slice = <TRow>(dimension: Dimension<TRow>): Function<Array<TRow>, Array<Array<TRow>>> =>
 	table => dimension.map(criteria => table.filter(row => criteria.every(criterion => criterion.predicate(row))));
 
