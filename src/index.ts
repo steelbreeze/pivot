faa743ab6a4eb883eab6454024e493de5ee5c4ef
David Mesquita-Morris <mesmo@steelbreeze.net>
2022-05-01 13:55:21 +0100

    Simplify distinct method

--- src/index.ts
+++ src/index.ts
@@ -38,8 +38,7 @@ export type Cube<TValue> = Array<Array<Array<TValue>>>;
  * @returns Returns the distinct set of values for the key
  */
 export const distinct = <TRow extends Row>(table: Array<TRow>, key: Key, getValue: Callback<TRow, any> = row => row[key]): Array<any> =>
-	[...table.reduce((result, row, index) => result.add(getValue(row, index, table)), new Set<any>())];
-
+	[...new Set(table.map(getValue))];
 /**
  * Creates a dimension from an array of values.
  * @param values A distinct list of values for the dimension.
