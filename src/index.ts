94f6ee54662359f8bcaa857df62a14a5032cf1a6
David Mesquita-Morris <mesmo@steelbreeze.net>
2023-04-04 10:27:48 +0100

    Better commenting of pivotImplementation

--- src/index.ts
+++ src/index.ts
@@ -24,14 +24,20 @@ export const distinct = <TSource>(value: TSource, index: number, source: Array<T
 export const criteria = <TSource>(key: keyof TSource): Function<TSource[keyof TSource], Predicate<TSource>> =>
 	(criterion: TSource[keyof TSource]) => (value: TSource) => value[key] === criterion;
 
-// internal implemntation of the pivot function. Note, this needs to be seperate from the external API as the recursion within does not use the external API.
+// internal implemntation of the pivot function. Note, this needs to be seperate from the external API as the recursion within does not use the external API; it also destroys the source passed in.
 const pivotImplementation = <TSource>(source: Array<TSource>, first: Dimension<TSource>, second?: Dimension<TSource>, ...others: Array<Dimension<TSource>>): Matrix<any> =>
+	// evaluate each predicate of the first dimension
 	first.map(predicate => {
-		let length = 0;
-		let slice = source.filter(record => predicate(record) || !(source[length++] = record));
+		// count of records remaining after those that match the predicate are filtered out
+		let remaining = 0;
 
-		source.length = length;
+		// perform the filter and repack records that do not match the predicate at the start of the source
+		let slice = source.filter(record => predicate(record) || !(source[remaining++] = record));
 
+		// update the source length to just the remaining records, thereby eliminating the reevaluation of items filtered out on subsiquent iterations
+		source.length = remaining;
+
+		// if there are more dimensions recurse, otherwise just return the slice
 		return second ? pivotImplementation(slice, second, ...others) : slice;
 	});
 
