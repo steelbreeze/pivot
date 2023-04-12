0d5120c8b0cf42c43431c2b3c5e5dd847f212eeb
David Mesquita-Morris <mesmo@steelbreeze.net>
2023-04-12 14:13:29 +0100

    Use TValue in place of TSource; improve comments and docs

--- lib/node/index.d.ts
+++ lib/node/index.d.ts
@@ -1,47 +1,52 @@
-/** A predicate is a simple function returning a boolean result */
+/** A simple function taking an agrument and returning a result */
 export declare type Function<TArg, TResult> = (arg: TArg) => TResult;
-/** A predicate is a simple function returning a boolean result */
+/** A predicate is a function returning a boolean result */
 export declare type Predicate<TArg> = Function<TArg, boolean>;
 /** A dimension is a series of predicates used to partition data. */
-export declare type Dimension<TSource> = Array<Predicate<TSource>>;
+export declare type Dimension<TValue> = Array<Predicate<TValue>>;
 /** A matrix is a two dimensional data structure. */
-export declare type Matrix<TSource> = Array<Array<TSource>>;
+export declare type Matrix<TValue> = Array<Array<TValue>>;
 /** A cube is a three dimensional data structure. */
-export declare type Cube<TSource> = Array<Array<Array<TSource>>>;
+export declare type Cube<TValue> = Array<Array<Array<TValue>>>;
 /** Function to pass into Array.prototype.filter to return unique values */
-export declare const distinct: <TSource>(value: TSource, index: number, source: TSource[]) => boolean;
+export declare const distinct: <TValue>(value: TValue, index: number, source: TValue[]) => boolean;
 /**
  * Create a callback to used in a map operation to create the predicate for each point on a dimension from a set of simple values.
+ * @typeParam TValue The type of the source data.
  * @param key The property in the source data to base this predicate on.
  */
-export declare const criteria: <TSource>(key: keyof TSource) => Function<TSource[keyof TSource], Predicate<TSource>>;
+export declare const criteria: <TValue>(key: keyof TValue) => Function<TValue[keyof TValue], Predicate<TValue>>;
 /**
  * Pivots source data by one or more dimensions returning an n-cube.
+ * @typeParam TValue The type of the source data.
  * @param source The source data, an array of objects.
  * @param first The first dimension used to pivot the source data.
  * @param second The second dimension used to pivot the source data.
  * @param third The third dimension used to pivot the source data.
  * @param others Additional dimensions to pivot the source data by.
- * @returns Returns an n-cube, the type of which depends on how many dimensions are passed in: Matrix<TSource> for one dimension; Cube<TSource> for two dimension; Cube<Array<TSource> for three dimensions, etc..
+ * @returns Returns an n-cube, the type of which depends on how many dimensions are passed in: Matrix<TValue> for one dimension; Cube<TValue> for two dimension; Cube<Array<TValue> for three dimensions, etc..
  */
 export declare const pivot: {
-    <TSource>(source: Array<TSource>, first: Dimension<TSource>): Matrix<TSource>;
-    <TSource>(source: Array<TSource>, first: Dimension<TSource>, second: Dimension<TSource>): Cube<TSource>;
-    <TSource>(source: Array<TSource>, first: Dimension<TSource>, second: Dimension<TSource>, third: Dimension<TSource>, ...others: Array<Dimension<TSource>>): Cube<Array<any>>;
+    <TValue>(source: Array<TValue>, first: Dimension<TValue>): Matrix<TValue>;
+    <TValue>(source: Array<TValue>, first: Dimension<TValue>, second: Dimension<TValue>): Cube<TValue>;
+    <TValue>(source: Array<TValue>, first: Dimension<TValue>, second: Dimension<TValue>, third: Dimension<TValue>, ...others: Array<Dimension<TValue>>): Cube<Array<any>>;
 };
 /**
  * Queries data from a cube; data previously pivoted by two dimensions.
+ * @typeParam TValue The type of the source data.
  * @param cube The cube to query data from.
  * @param query A callback function to create a result from each cell of the cube.
  */
-export declare const map: <TSource, TResult>(cube: Cube<TSource>, query: Function<TSource[], TResult>) => Matrix<TResult>;
+export declare const map: <TValue, TResult>(cube: Cube<TValue>, query: Function<TValue[], TResult>) => Matrix<TResult>;
 /**
  * A generator, to create a function to pass into a cube map operation as the query parameter that sums numerical values derived from rows in a cube.
+ * @typeParam TValue The type of the source data.
  * @param selector A callback function to derive a numerical value for each object in the source data.
  */
-export declare const sum: <TSource>(selector: Function<TSource, number>) => Function<TSource[], number>;
+export declare const sum: <TValue>(selector: Function<TValue, number>) => Function<TValue[], number>;
 /**
  * A generator, to create a function to pass into query that averages numerical values derived from rows in a cube.
+ * @typeParam TValue The type of the source data.
  * @param selector A callback function to derive a numerical value for each object in the source data.
  */
-export declare const average: <TSource>(selector: Function<TSource, number>) => Function<TSource[], number>;
+export declare const average: <TValue>(selector: Function<TValue, number>) => Function<TValue[], number>;
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,62 @@
-/** A predicate is a simple function returning a boolean result */
+/** A simple function taking an agrument and returning a result */
 export type Function<TArg, TResult> = (arg: TArg) => TResult;
 
-/** A predicate is a simple function returning a boolean result */
+/** A predicate is a function returning a boolean result */
 export type Predicate<TArg> = Function<TArg, boolean>;
 
 /** A dimension is a series of predicates used to partition data. */
-export type Dimension<TSource> = Array<Predicate<TSource>>;
+export type Dimension<TValue> = Array<Predicate<TValue>>;
 
 /** A matrix is a two dimensional data structure. */
-export type Matrix<TSource> = Array<Array<TSource>>;
+export type Matrix<TValue> = Array<Array<TValue>>;
 
 /** A cube is a three dimensional data structure. */
-export type Cube<TSource> = Array<Array<Array<TSource>>>;
+export type Cube<TValue> = Array<Array<Array<TValue>>>;
 
 /** Function to pass into Array.prototype.filter to return unique values */
-export const distinct = <TSource>(value: TSource, index: number, source: Array<TSource>): boolean =>
+export const distinct = <TValue>(value: TValue, index: number, source: Array<TValue>): boolean =>
 	source.indexOf(value) === index;
 
 /**
  * Create a callback to used in a map operation to create the predicate for each point on a dimension from a set of simple values.
+ * @typeParam TValue The type of the source data.
  * @param key The property in the source data to base this predicate on.
  */
-export const criteria = <TSource>(key: keyof TSource): Function<TSource[keyof TSource], Predicate<TSource>> =>
-	(criterion: TSource[keyof TSource]) => (value: TSource) => value[key] === criterion;
+export const criteria = <TValue>(key: keyof TValue): Function<TValue[keyof TValue], Predicate<TValue>> =>
+	(criterion: TValue[keyof TValue]) => (value: TValue) => value[key] === criterion;
 
 /**
  * Pivots source data by one or more dimensions returning an n-cube.
+ * @typeParam TValue The type of the source data.
  * @param source The source data, an array of objects.
  * @param first The first dimension used to pivot the source data.
  * @param second The second dimension used to pivot the source data.
  * @param third The third dimension used to pivot the source data.
  * @param others Additional dimensions to pivot the source data by.
- * @returns Returns an n-cube, the type of which depends on how many dimensions are passed in: Matrix<TSource> for one dimension; Cube<TSource> for two dimension; Cube<Array<TSource> for three dimensions, etc..
+ * @returns Returns an n-cube, the type of which depends on how many dimensions are passed in: Matrix<TValue> for one dimension; Cube<TValue> for two dimension; Cube<Array<TValue> for three dimensions, etc..
  */
 export const pivot: {
-	<TSource>(source: Array<TSource>, first: Dimension<TSource>): Matrix<TSource>;
-	<TSource>(source: Array<TSource>, first: Dimension<TSource>, second: Dimension<TSource>): Cube<TSource>;
-	<TSource>(source: Array<TSource>, first: Dimension<TSource>, second: Dimension<TSource>, third: Dimension<TSource>, ...others: Array<Dimension<TSource>>): Cube<Array<any>>;
-} = <TSource>(source: Array<TSource>, first: Dimension<TSource>, second?: Dimension<TSource>, ...others: Array<Dimension<TSource>>): Matrix<any> => pivotImplementation([...source], first, second, ...others);
+	<TValue>(source: Array<TValue>, first: Dimension<TValue>): Matrix<TValue>;
+	<TValue>(source: Array<TValue>, first: Dimension<TValue>, second: Dimension<TValue>): Cube<TValue>;
+	<TValue>(source: Array<TValue>, first: Dimension<TValue>, second: Dimension<TValue>, third: Dimension<TValue>, ...others: Array<Dimension<TValue>>): Cube<Array<any>>;
+} = <TValue>(source: Array<TValue>, first: Dimension<TValue>, second?: Dimension<TValue>, ...others: Array<Dimension<TValue>>): Matrix<any> => pivotImplementation([...source], first, second, ...others);
 
 // implemntation of the pivot function. Note, this needs to be seperate from the external API as the recursion within does not use the external API; it also destroys the source passed in.
-const pivotImplementation = <TSource>(source: Array<TSource>, first: Dimension<TSource>, second?: Dimension<TSource>, ...others: Array<Dimension<TSource>>): Matrix<any> =>
+const pivotImplementation = <TValue>(source: Array<TValue>, first: Dimension<TValue>, second?: Dimension<TValue>, ...others: Array<Dimension<TValue>>): Matrix<any> =>
 	second ? first.map(predicate => pivotImplementation(slice(source, predicate), second, ...others)) : first.map(predicate => slice(source, predicate));
 
 // slice an array in two, returning records that match the predicate and removing them from the source.
-const slice = <TSource>(source: Array<TSource>, predicate: Predicate<TSource>): Array<TSource> => {
-	const filtered: Array<TSource> = [];
+const slice = <TValue>(source: Array<TValue>, predicate: Predicate<TValue>): Array<TValue> => {
+	const result: Array<TValue> = [];
 	let remaining: number = 0;
 
-	for (let index: number = 0; index < source.length; ++index) {
-		const record: TSource = source[index];
+	for (let i = 0; i < source.length; ++i) {
+		const record: TValue = source[i];
 
 		if (predicate(record)) {
-			filtered.push(record);
+			result.push(record);
 		} else {
-			if (index !== remaining) {
+			if (i !== remaining) {
 				source[remaining] = record;
 			}
 
@@ -64,27 +66,30 @@ const slice = <TSource>(source: Array<TSource>, predicate: Predicate<TSource>): 
 
 	source.length = remaining;
 
-	return filtered;
+	return result;
 }
 
 /**
  * Queries data from a cube; data previously pivoted by two dimensions.
+ * @typeParam TValue The type of the source data.
  * @param cube The cube to query data from.
  * @param query A callback function to create a result from each cell of the cube.
  */
-export const map = <TSource, TResult>(cube: Cube<TSource>, query: Function<Array<TSource>, TResult>): Matrix<TResult> =>
-	cube.map((matrix: Matrix<TSource>) => matrix.map(query));
+export const map = <TValue, TResult>(cube: Cube<TValue>, query: Function<Array<TValue>, TResult>): Matrix<TResult> =>
+	cube.map((matrix: Matrix<TValue>) => matrix.map(query));
 
 /**
  * A generator, to create a function to pass into a cube map operation as the query parameter that sums numerical values derived from rows in a cube.
+ * @typeParam TValue The type of the source data.
  * @param selector A callback function to derive a numerical value for each object in the source data.
  */
-export const sum = <TSource>(selector: Function<TSource, number>): Function<Array<TSource>, number> =>
-	(source: Array<TSource>) => source.reduce((total: number, source: TSource) => total + selector(source), 0);
+export const sum = <TValue>(selector: Function<TValue, number>): Function<Array<TValue>, number> =>
+	(source: Array<TValue>) => source.reduce((total: number, source: TValue) => total + selector(source), 0);
 
 /**
  * A generator, to create a function to pass into query that averages numerical values derived from rows in a cube.
+ * @typeParam TValue The type of the source data.
  * @param selector A callback function to derive a numerical value for each object in the source data.
  */
-export const average = <TSource>(selector: Function<TSource, number>): Function<Array<TSource>, number> =>
-	(source: Array<TSource>) => sum(selector)(source) / source.length;
+export const average = <TValue>(selector: Function<TValue, number>): Function<Array<TValue>, number> =>
+	(source: Array<TValue>) => sum(selector)(source) / source.length;
