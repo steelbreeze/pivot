4703db2bf93ff5f8441364d708f65706604aa5b7
David Morris <david.morris@me.com>
2024-12-08 15:11:50 +0000

    Simplify example typing

--- src/example/index.ts
+++ src/example/index.ts
@@ -1,26 +1,26 @@
-import { Function, Dimension, Cube, criteria, pivot, aggregate, average, Matrix } from '..';
+import { criteria, pivot, aggregate, average } from '..';
 import { Player, squad } from './fulham';
 import { distinct } from './distinct';
 
-// the position dimension we want in a custom order
-const positions: string[] = ['Goalkeeper', 'Defender', 'Midfielder', 'Forward'];
+// the position dimension we want in a defined order
+const positions = ['Goalkeeper', 'Defender', 'Midfielder', 'Forward'];
 
 // the countries dimension we derive from the squad data and order alphabetically
-const countries: string[] = squad.map(player => player.country).filter(distinct).sort();
+const countries = squad.map(player => player.country).filter(distinct).sort();
 
 // we then create dimensions which also reference a property in the source data 
-const x: Dimension<Player> = positions.map(criteria('position'));
-const y: Dimension<Player> = countries.map(criteria('country'));
+const x = positions.map(criteria('position'));
+const y = countries.map(criteria('country'));
 
 // create the pivot cube from the squad data using position and country for x and y axes
-const cube: Cube<Player> = pivot(squad, y, x);
+const cube = pivot(squad, y, x);
 
 // find the average age of players by position by country as at 2021-05-23
-const result: Matrix<number> = aggregate(cube, average(age(new Date('2021-05-23'))));
+const result = aggregate(cube, average(age(new Date('2021-05-23'))));
 
 // Creates a callback to calculate a players age from their date of birth as at a given date
-function age(asAt: Date = new Date()): Function<Player, number> {
-	return player => new Date(asAt.getTime() - player.dateOfBirth.getTime()).getUTCFullYear() - 1970;
+function age(asAt: Date = new Date()) {
+	return (player: Player) => new Date(asAt.getTime() - player.dateOfBirth.getTime()).getUTCFullYear() - 1970;
 }
 
 // pretty print the result with axes
