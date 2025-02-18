1bf8dc62acfcf6b2338433a4aded99331c1087d3
David Morris <david.morris@me.com>
2025-02-18 00:23:17 +0000

    Do not emit comments in .js files

--- lib/node/example/distinct.d.ts
+++ lib/node/example/distinct.d.ts
@@ -1,4 +1,1 @@
-/**
- * Function to pass into Array.prototype.filter to return unique values.
- */
 export declare function distinct<TValue>(value: TValue, index: number, source: Array<TValue>): boolean;
